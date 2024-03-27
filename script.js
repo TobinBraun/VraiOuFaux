83e179680033307ae87f3549db515e1a034f19a2
TobinBraun <93841411+TobinBraun@users.noreply.github.com>
2024-03-27 15:42:21 -0400

    question class + changed game for civics project

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-const answers = ["f", "f", "v", "f"];
+//const answers = ["f", "f", "v", "f"];
+
 let i = 0;
 score = 0;
 
+class Question {
+    constructor(q, a) {
+        this.question = q;
+        this.answer = a;
+    }
+}
+
+const questions = [
+    new Question("Mao Zedong a créé des problèmes de mobilité dans la chine", "v"), 
+    new Question("Les personnes qui a proposé un solution des problèmes n’est pas dans la prison", "f"), 
+    new Question("Les camps de renseignement sont pour enseigner les ouïghours le Mandarin", "f"), 
+    new Question("En chine il y a un loi qui donne la liberté de presse.", "v"), 
+    new Question("Il y a 101 reporteurs en prison maintenant", "f"),
+    new Question("Le chambre de communs a décidé d’appeler les évenements au Xinjiang comme génocide", "v")];
+
 function answer(ans) {
     console.log(i)
-    if (ans == answers[i]) {
+    if (ans == questions[i].answer) {
         score++;
     }
     i++;
-    document.getElementById("question").innerHTML = `Question ${i+1}`
-    if (i >= 4) {
+    
+    if (i >= questions.length) {
         showScore();
     }
+    document.getElementById("question").innerHTML = questions[i].question;
 }
 
 function showScore() {
@@ -21,5 +38,5 @@ function showScore() {
     document.getElementById("question").style.display = "none";
     const scoreText = document.getElementById("scoreText");
     scoreText.style.display = "block";
-    scoreText.innerHTML = `Score: ${score}/4`
+    scoreText.innerHTML = `Score: ${score}/${questions.length}`
 }
